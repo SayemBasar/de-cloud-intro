23339628e18fdd6e32a2c156da1735352adaf5ab
Md. Sayem Basar <sayemb.cse@gmail.com>
2025-10-27 11:57:40 +0000

    Access the both env

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     environment = os.getenv("ENVIRONMENT", "unknown")
-    return f"Hello Sayem from the {environment} environment!"
+    db_url=os.getenv("DATABASE_URL")
+    return f"<H2>Hello Sayem from the {environment} environment!</h2><p> DB URL: {db_url}</p>"
 
 if __name__ =="__main__":
     app.run(debug=True)
